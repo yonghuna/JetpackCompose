a6c5a20f3fa9408af0410c7114af276ecac99862
YONGHUN <kooaas@daum.net>
2022-08-11 21:56:51 +0900

    90 %

--- app/src/main/java/com/example/jetpactutorial/MainActivity.kt
+++ app/src/main/java/com/example/jetpactutorial/MainActivity.kt
@@ -1,5 +1,8 @@
 package com.example.jetpactutorial
 
+import android.annotation.SuppressLint
+import android.graphics.Bitmap
+import android.graphics.drawable.Drawable
 import android.os.Bundle
 import android.text.Layout
 import android.widget.ListView
@@ -12,12 +15,15 @@ import androidx.compose.foundation.lazy.LazyItemScope
 import androidx.compose.foundation.lazy.items
 import androidx.compose.foundation.lazy.rememberLazyListState
 import androidx.compose.foundation.shape.CircleShape
+import androidx.compose.foundation.shape.RoundedCornerShape
 import androidx.compose.material.*
 import androidx.compose.material.icons.Icons
 import androidx.compose.material.icons.filled.ArrowBack
 import androidx.compose.material.icons.filled.Favorite
 import androidx.compose.material.icons.filled.Search
 import androidx.compose.runtime.Composable
+import androidx.compose.runtime.MutableState
+import androidx.compose.runtime.mutableStateOf
 import androidx.compose.ui.Alignment
 import androidx.compose.ui.Alignment.Companion.BottomCenter
 import androidx.compose.ui.Alignment.Companion.CenterVertically
@@ -26,14 +32,20 @@ import androidx.compose.ui.Alignment.Companion.TopStart
 import androidx.compose.ui.Modifier
 import androidx.compose.ui.draw.clip
 import androidx.compose.ui.graphics.Color
+import androidx.compose.ui.graphics.asImageBitmap
 import androidx.compose.ui.layout.ContentScale
+import androidx.compose.ui.platform.LocalContext
 import androidx.compose.ui.res.painterResource
 import androidx.compose.ui.text.TextStyle
 import androidx.compose.ui.text.font.FontWeight
+import androidx.compose.ui.text.style.TextAlign
 import androidx.compose.ui.tooling.preview.Preview
 import androidx.compose.ui.unit.dp
 import androidx.compose.ui.unit.sp
 import coil.compose.rememberImagePainter
+import com.bumptech.glide.Glide
+import com.bumptech.glide.request.target.CustomTarget
+import com.bumptech.glide.request.transition.Transition
 import com.example.jetpactutorial.ui.theme.JetpacTutorialTheme
 
 class MainActivity : ComponentActivity() {
@@ -43,22 +55,22 @@ class MainActivity : ComponentActivity() {
             JetpacTutorialTheme {
                 // A surface container using the 'background' color from the theme
                 Surface(color = MaterialTheme.colors.background) {
-                    //ListListScopeSample()
-                    //SimpleListView()
-//                    fruitsList.add(FruitModel("Apple", R.drawable.chiken))
-//                    fruitsList.add(FruitModel("Orange", R.drawable.chiken))
-//                    fruitsList.add(FruitModel("Banana", R.drawable.chiken))
-//                    fruitsList.add(FruitModel("Strawberry", R.drawable.chiken))
-//                    fruitsList.add(FruitModel("Mango", R.drawable.chiken))
-//                    LazyColumn(
-//                        modifier = Modifier
-//                            .fillMaxSize()
-//                            .background(Color.White)
-//                    ) {
-//                        items(fruitsList) { model ->
-//                            ListRow(model = model)
-//                        }
-//                    }
+                    TopAppBar(
+                        title = {
+                            Text(text = "AppBar")
+                            JetpacTutorialTheme {
+                                LazyColumn(
+                                ) {
+                                    items(10) {
+                                        KotlinWorldCard(order = it)
+                                    }
+
+                                }
+
+                            }
+                        }
+
+                    )
 
                 }
             }
@@ -67,19 +79,21 @@ class MainActivity : ComponentActivity() {
 }
 
 
-
-
-
 @Composable
 fun KotlinWorldCard(order: Int) {
     Card(
         Modifier
-            .padding(12.dp)
-            .border(width = 4.dp, color = Color.Black)
+            .padding(10.dp)
             .fillMaxWidth()
-            .height(60.dp)
+            .height(60.dp),
+        elevation = 10.dp,
+        shape = RoundedCornerShape(12.dp)
     ) {
-        Box(contentAlignment = Alignment.Center) {
+        Row(
+            modifier = Modifier.padding(10.dp),
+            verticalAlignment = Alignment.CenterVertically,
+            horizontalArrangement = Arrangement.spacedBy(10.dp)
+        ) {
             Image(
                 painter = painterResource(R.drawable.chiken),
                 contentDescription = "",
@@ -88,47 +102,100 @@ fun KotlinWorldCard(order: Int) {
                     .clip(CircleShape)
                     .padding(5.dp)
                     .size(60.dp)
-                    .align(Alignment.TopStart)
-
             )
+        }
+
+        Column(
+            modifier = Modifier
+                .padding(8.dp),
+            horizontalAlignment = Alignment.CenterHorizontally,
+        ) {
             Text(
-                modifier = Modifier
-                    .align(Alignment.TopCenter),
                 text = "개발하는 LEE",
                 fontSize = 20.sp,
                 fontWeight = FontWeight.SemiBold,
-                color = Color.White
+                color = Color.Black
             )
             Text(
-                modifier = Modifier
-                    .align(Alignment.BottomCenter),
                 text = "일해라",
                 fontSize = 12.sp,
                 fontWeight = FontWeight.SemiBold,
-                color = Color.White
+                color = Color.Black
             )
 
 
         }
     }
-
-
 }
 
+@SuppressLint("UnrememberedMutableState")
+@Composable
+fun ProfileImg(imgUrl: String, modifier: Modifier = Modifier) {
+
+    val bitmap: MutableState<Bitmap?> = mutableStateOf(null)
+
+    Glide.with(LocalContext.current)
+        .asBitmap()
+        .load(imgUrl)
+        .into(object : CustomTarget<Bitmap>() {
+            override fun onResourceReady(resource: Bitmap, transition: Transition<in Bitmap>?) {
+                bitmap.value = resource
+            }
+
+            override fun onLoadCleared(placeholder: Drawable?) {
+            }
+
+        })
+
+    bitmap.value?.asImageBitmap()?.let {
+        Image(
+            bitmap = it,
+            contentScale = ContentScale.Fit,
+            contentDescription = null,
+            modifier = modifier
+                .size(width = 50.dp, 50.dp)
+                .clip(RoundedCornerShape(10.dp))
+        )
+    } ?: Image(
+        painter = painterResource(id = R.drawable.chiken),
+        contentScale = ContentScale.Fit,
+        contentDescription = null,
+        modifier = modifier
+            .size(width = 50.dp, 50.dp)
+            .clip(RoundedCornerShape(10.dp))
+    )
+
+}
 
 @Preview(showBackground = true)
 @Composable
 fun DefaultPreview() {
-    JetpacTutorialTheme {
+    Scaffold(topBar = {
         TopAppBar(
-            title = { Text(text = "AppBar") }
-        )
+            elevation = 10.dp,
+            modifier = Modifier.height(58.dp)
+        ) {
+            Text(
+                text = "AppTopBar",
+                modifier = Modifier
+                    .padding(8.dp)
+                    .align(Alignment.CenterVertically),
+                fontSize = 18.sp,
+                fontWeight = FontWeight.Black
+            )
+
+        }
+    }) {
         LazyColumn(
         ) {
-            items(10){
+            items(10) {
                 KotlinWorldCard(order = it)
             }
 
         }
     }
+
+
 }
+
+
